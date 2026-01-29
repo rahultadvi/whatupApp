353d1557f586ecba0aa3e52996ecd33b30ec5499
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-01-29 21:45:30 +0530

    url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import {
 } from "react-icons/fi";
 import { BsWhatsapp } from "react-icons/bs";
 
-const BASE_URL = "https://whatupapp-2.onrender.com"; 
+const BASE_URL = "https://whatupapp.onrender.com"; 
 function App() {
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
